fc12d0818525029446383470d7bba5984585ff8a
PhullFury <66832169+PhullFury@users.noreply.github.com>
2022-09-19 11:53:00 +0530

    Initial commit

---
+++ Source/Island/IslandGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "IslandGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ISLAND_API AIslandGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
